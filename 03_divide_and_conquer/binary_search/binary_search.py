adf53723e33075ef4c4af9c66899377f34dbc2d8
Aleksey Berezan <aleksey.berezan@gmail.com>
2017-01-29 22:36:26 -0500

    first two tasks initial attempts

--- 03_divide_and_conquer/binary_search/binary_search.py
+++ 03_divide_and_conquer/binary_search/binary_search.py
@@ -1,9 +1,21 @@
 # Uses python3
 import sys
+import random
 
 def binary_search(a, x):
-    left, right = 0, len(a)
-    # write your code here
+    left, right = 0, len(a)-1
+
+    while left <= right:
+        mid_index = (right + left) // 2
+        mid = a[mid_index]
+        if x == mid:
+            return mid_index
+        if x < mid:
+            right = mid_index - 1
+        else:
+            left = mid_index + 1
+
+    return -1
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -11,7 +23,58 @@ def linear_search(a, x):
             return i
     return -1
 
-if __name__ == '__main__':
+test = False
+
+def generate_test_case(size):
+    # size = 5
+    min_num = 0
+    max_num = 9999
+    a = [0] * size
+    for i in range(0, size):
+        a[i] = random.randint(min_num,max_num)
+    a.sort()
+    
+    index = random.randint(0, size * 2)
+    if index >= 0 and index < size:
+        x = a[index]
+    else:
+        x = max_num + 42
+
+    return (a, x)
+
+if test:
+    test_case_count = 4000
+    success = 0
+    failures = []
+
+    for i in range(0, test_case_count):
+        size = i % 3000
+        test_case = generate_test_case(size)
+        (a, x) = test_case
+        actual = binary_search(a, x)
+        correct = True
+        if actual == -1:
+            if x in a:
+                correct = False
+        elif actual < 0 or actual >= len(a) or a[actual] != x:
+            correct = False
+
+        if not (correct):
+            failures.append((test_case, actual))
+        else: 
+            success += 1
+
+    failed = len(failures)
+    print("Success: {0}, Failed: {1}".format(success, failed))
+    print("")
+    if failed == 0:
+        print("Success")
+    else:
+        print("FAILED!!!")
+        print("Failed Test Cases:")         
+        for ((a, x, index), actual) in failures:
+            print("%s | %s | %s | %s" % (a, x, index, actual))
+else:
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n = data[0]
@@ -19,4 +82,4 @@ if __name__ == '__main__':
     a = data[1 : n + 1]
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search(a, x), end = ' ')
--- 03_divide_and_conquer/majority_element/majority_element.py
+++ 03_divide_and_conquer/majority_element/majority_element.py
@@ -1,18 +1,116 @@
 # Uses python3
 import sys
+import random
+import itertools
+
+def get_sort_key(x):
+    return x[0]
+
+def merge(left, right):
+    r = { }
+    # print (left)
+    for x, count in left:
+        if x in r:
+            r[x] += count
+        else:
+            r[x] = count
+    for x, count in right:
+        if x in r:
+            r[x] += count
+        else:
+            r[x] = count
+    result = list(r.items())
+    # print("returning " + str(result))
+    return result
+
+def get_majority_element_internal(a, left, right):
+    if left > right:
+        return []
+
+    if left == right:
+        return [(a[left], 1)]
+    if left + 1 == right:
+        # print(a)
+        # print(left)
+        # print(right)
+        if a[left] == a[right]:
+            return [(a[left], 2)]
+        else:
+            return [(a[left], 1), (a[right], 1)]#.sort(key=get_sort_key)
+
+    mid = (left + right) // 2
+    left_results = get_majority_element_internal(a, left, mid)
+    right_results = get_majority_element_internal(a, mid + 1, right)
+    return merge(left_results, right_results)
 
 def get_majority_element(a, left, right):
-    if left == right:
+    merged = get_majority_element_internal(a, left, right)
+    merged.sort(key=lambda x:x[1],reverse=True)
+    # print("we've got: " +str(merged))
+    if len(merged) == 0:
         return -1
-    if left + 1 == right:
-        return a[left]
+    top_item,top_count = merged[0]
+    return 1 if top_count > len(a) // 2 else -1
+
+    # if right - left == 1:
+    #     return 
+    # if left == right:
+    #     return -1
+    # if left + 1 == right:
+    #     return a[left]
     #write your code here
     return -1
 
-if __name__ == '__main__':
+def generate_test_array(size, has_majority_item):
+    if size == 0:
+        return []
+    # size = 5
+    min_num = 12
+    max_num = 99
+    majority_num = 11
+    a = [0] * size
+    for i in range(0, size):
+        a[i] = random.randint(min_num,max_num)
+    
+    if has_majority_item:
+        for i in range(0, (size // 2) + 1):
+            a[i] = majority_num
+    
+    perms = list(itertools.permutations(a))
+    return list(perms)[random.randint(0, len(perms)-1)]
+
+test = False
+if test:
+    test_case_count = 100
+    success = 0
+    failures = []
+
+    for i in range(0, test_case_count):
+        size = max(1, i % 9)
+        has_majority_item = i % 2 == 0
+        test_array = generate_test_array(size, has_majority_item)
+        a = test_array
+        actual = get_majority_element(a, 0, len(a)-1)
+        expected = 1 if has_majority_item else -1
+        if actual != expected:
+            failures.append((test_array, expected, actual))
+        else: 
+            success += 1
+
+    failed = len(failures)
+    print("Success: {0}, Failed: {1}".format(success, failed))
+    print("")
+    if failed == 0:
+        print("Success")
+    else:
+        print("FAILED!!!")
+        print("Failed Test Cases:")         
+        for test_array,expected,actual in failures:
+            print("%s | %s | %s" % (test_array,expected,actual))
+else:
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
+    if get_majority_element(a, 0, n-1) != -1:
         print(1)
     else:
         print(0)
