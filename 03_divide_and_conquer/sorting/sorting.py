7cc186755ceb5871e14947a96bab44b1806a2bba
Aleksey Berezan <aleksey.berezan@gmail.com>
2017-01-31 01:24:44 -0500

    q sort

--- 03_divide_and_conquer/sorting/sorting.py
+++ 03_divide_and_conquer/sorting/sorting.py
@@ -2,35 +2,31 @@
 import sys
 import random
 
-def partition3(a, l, r):
-    #write your code here
-    pass
-
-def partition2(a, l, r):
-    x = a[l]
-    j = l;
-    for i in range(l + 1, r + 1):
-        if a[i] <= x:
-            j += 1
-            a[i], a[j] = a[j], a[i]
-    a[l], a[j] = a[j], a[l]
-    return j
-
-
 def randomized_quick_sort(a, l, r):
     if l >= r:
         return
-    k = random.randint(l, r)
-    a[l], a[k] = a[k], a[l]
-    #use partition3
-    m = partition2(a, l, r)
-    randomized_quick_sort(a, l, m - 1);
-    randomized_quick_sort(a, m + 1, r);
+    pivot = a[random.randint(l, r)]
+    li,ri = l,r
+    i = li
+    while i <= ri:
+        if a[i] < pivot:
+            a[li], a[i] = a[i], a[li]
+            i += 1
+            li += 1
+            continue
+        if a[i] == pivot:
+            i += 1
+            continue
+        if a[i] > pivot:
+            a[ri], a[i] = a[i], a[ri]
+            ri -= 1
+            continue
+    
+    randomized_quick_sort(a, l, li - 1)
+    randomized_quick_sort(a, ri + 1, r)
 
-
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *a = list(map(int, input.split()))
-    randomized_quick_sort(a, 0, n - 1)
-    for x in a:
-        print(x, end=' ')
+input = sys.stdin.read()
+n, *a = list(map(int, input.split()))
+randomized_quick_sort(a, 0, n - 1)
+for x in a:
+    print(x, end=' ')
