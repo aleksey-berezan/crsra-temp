d1116fbe0e045f079ae3f6ddd123137ef2e2df46
Aleksey Berezan <aleksey.berezan@gmail.com>
2017-02-04 20:42:58 -0500

    inversions

--- 03_divide_and_conquer/inversions/inversions.py
+++ 03_divide_and_conquer/inversions/inversions.py
@@ -1,18 +1,45 @@
 # Uses python3
 import sys
 
-def get_number_of_inversions(a, b, left, right):
-    number_of_inversions = 0
-    if right - left <= 1:
-        return number_of_inversions
-    ave = (left + right) // 2
-    number_of_inversions += get_number_of_inversions(a, b, left, ave)
-    number_of_inversions += get_number_of_inversions(a, b, ave, right)
-    #write your code here
-    return number_of_inversions
+def split(array):
+    m = len(array) // 2
+    return (array[:m], array[m:])
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *a = list(map(int, input.split()))
-    b = n * [0]
-    print(get_number_of_inversions(a, b, 0, len(a)))
+def merge(left, right, inversions_acc):
+    # print("Merging {0} - {1}. Inversions in: {2}".format(left, right, inversions_acc))
+    result = [0] * (len(left) + len(right))
+    l, r = 0, 0
+    inversions = 0
+    for i in range(0, len(result)):
+        if l >= len(left):
+            result[i] = right[r]
+            r += 1
+            continue
+        if r >= len(right):
+            result[i] = left[l]
+            l += 1
+            continue
+        if left[l] <= right[r]:
+            result[i] = left[l]
+            l += 1
+        else:
+            result[i] = right[r]
+            r += 1
+            inversions += (len(left) - l)
+    inversions_acc += inversions
+    # print("Inversions added: {0}, Inversions out: {1}".format(inversions, inversions_acc))
+    return result, inversions_acc
+
+def merge_sort(array):
+    if len(array) <= 1:
+        return (array, 0)
+
+    left, right = split(array)
+    (left_sorted, li) = merge_sort(left)
+    (right_sorted, ri) = merge_sort(right)
+    return merge(left_sorted, right_sorted, li + ri)
+
+input = sys.stdin.read()
+n, *a = list(map(int, input.split()))
+(sorted, inversions) = merge_sort(a)
+print(inversions)
