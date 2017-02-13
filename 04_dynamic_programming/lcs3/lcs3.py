594996189f803ced264b396d8fd87a2f8aa574a1
Aleksey Berezan <aleksey.berezan@gmail.com>
2017-02-12 19:58:08 -0500

    1 test case not passing in lcs3

--- 04_dynamic_programming/lcs3/lcs3.py
+++ 04_dynamic_programming/lcs3/lcs3.py
@@ -2,57 +2,76 @@
 
 import sys
 
-def get_edit_distance_matrix(left, right):
-    n = len(right)
-    m = len(left)
-    d = [0] * (n + 1)
-    for j in range(0, n + 1):
-        d[j] = [0] * (m + 1)
+def get_edit_distance_matrix(first, second, third):
+    m = len(first)
+    n = len(second)
+    p = len(third)
+
+    d = [0] * (p + 1)#[0] * (n + 1)
+    for k in range(0, p + 1):
+        d[k] = [0] * (n + 1)
+        for j in range(0, n + 1):
+            d[k][j] = [0] * (m + 1)
         #d[j][0] = j
     #for i in range(0, m + 1):
         #d[0][i] = i
-
-    for j in range(1, m + 1):
-        for i in range(1, n + 1):
-            if right[i - 1] == left[j - 1]:
-                d[i][j] = d[i - 1][j - 1] + 1
-            else:
-                insertion = d[i][j - 1]
-                deletion = d[i - 1][j]
-                # mismatch = d[i - 1][j - 1]
-                d[i][j] = max(insertion, deletion)#, mismatch)
+    for k in range(1, p + 1):
+        for j in range(1, m + 1):
+            for i in range(1, n + 1):
+                if second[i - 1] == first[j - 1] == third[k - 1]:
+                    d[k][i][j] = d[k - 1][i - 1][j - 1] + 1
+                else:
+                    insertion1 = d[k][i][j - 1]
+                    deletion1 = d[k][i - 1][j]
+                    insertion2 = d[k-1][i][j - 1]
+                    deletion2 = d[k-1][i - 1][j]
+                    # mismatch = d[i - 1][j - 1]
+                    d[k][i][j] = max(insertion1, deletion1, insertion2, deletion2)#, mismatch)
 
     #return d[n][m]
-    # backtrack
 
-    common = [None] * d[n][m]
-    c_index = d[n][m] - 1
-    i, j = n, m
-    while i > 0 and j > 0:
-        if left[j - 1] == right[i - 1]:#d[i][j] > d[i-1][j-1]:
-            common[c_index], c_index = left[j - 1], c_index - 1
-            i,j = i - 1, j - 1
+    # backtrack
+    common = [None] * d[p][n][m]
+    c_index = d[p][n][m] - 1
+    k, i, j = p, n, m
+    while k > 0 and i > 0 and j > 0:
+        if first[j - 1] == second[i - 1] == third[k - 1]:#d[i][j] > d[i-1][j-1]:
+            common[c_index], c_index = first[j - 1], c_index - 1
+            k, i, j = k - 1, i - 1, j - 1
         else:
-            if d[i-1][j] > d[i][j]-1:
-                i -= 1
-            else:
-                j -= 1
+            options = [
+                (d[k-1][i][j], (k-1,i,j)),
+                (d[k][i-1][j], (k,i-1,j)),
+                (d[k][i][j-1], (k,i,j-1)),
+            ]
+            options.sort(key=lambda x: x[0], reverse=True)
+            k,i,j = options[0][1]
+            # if d[i-1][j] > d[i][j]-1:
+            #     i -= 1
+            # else:
+            #     j -= 1
 
     return common
     #return d
 
 def lcs3(a, b, c):
-    common = get_edit_distance_matrix(a,b)
-
-    #write your code here
-    return min(len(a), len(b), len(c))
+    common = get_edit_distance_matrix(a,b,c)
+    return common
+    # return "".join([str(x) for x in common])
 
 test = True
 if test:
-    test_cases = [("abcdaf", "acbcf", "")
-        ]
-    for (a,b,c) in test_cases:
+    test_cases = [#("abcdaf", "acbcf", ""),
+                  ((['a', 'b', 'c', 'd', 'a', 'f'], ['a', 'c', 'b', 'c', 'f'], ['a', 'b', 'c', 'd', 'a', 'f']), ['a','b','c','f']),
+                  ((['a', 'b', 'c', 'd', 'a', 'f'], ['a', 'c', 'b', 'c', 'f'], ['a', 'c', 'b', 'c', 'f']), ['a','b','c','f']),
+                  ((list("123"), list("213"), list("135")), list("13")),
+                  (([8, 3, 2, 1, 7], [8, 2, 1, 3, 8, 10, 7], [6, 8, 3, 1, 4, 7]), [8, 3, 7])]
+    for (a,b,c),expected in test_cases:
         actual = lcs3(a,b,c)
+        if actual != expected:
+            print("FAIL. Expected: {0}, Actual: {1} for {2}".format(expected, actual, (a,b,c)))
+        else:
+            print("SUCCESS")
 else:
     #main
     #if __name__ == '__main__':
