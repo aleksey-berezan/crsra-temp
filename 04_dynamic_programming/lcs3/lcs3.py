40213fce663fe20b5c483ed3f04f077222e8571e
Aleksey Berezan <aleksey.berezan@gmail.com>
2017-02-12 20:30:36 -0500

    removed redundand stuff from lc3

--- 04_dynamic_programming/lcs3/lcs3.py
+++ 04_dynamic_programming/lcs3/lcs3.py
@@ -2,100 +2,67 @@
 
 import sys
 
-def get_edit_distance_matrix(first, second, third):
+def lcs3(first, second, third):
     m = len(first)
     n = len(second)
     p = len(third)
 
-    d = [0] * (p + 1)#[0] * (n + 1)
+    # init
+    d = [0] * (p + 1)
     for k in range(0, p + 1):
         d[k] = [0] * (n + 1)
         for j in range(0, n + 1):
             d[k][j] = [0] * (m + 1)
-        #d[j][0] = j
-    #for i in range(0, m + 1):
-        #d[0][i] = i
+
+    # fill
     for k in range(1, p + 1):
         for j in range(1, m + 1):
             for i in range(1, n + 1):
                 if second[i - 1] == first[j - 1] == third[k - 1]:
                     d[k][i][j] = d[k - 1][i - 1][j - 1] + 1
                 else:
-                    insertion1 = d[k][i][j - 1]
-                    deletion1 = d[k][i - 1][j]
-                    insertion2 = d[k-1][i][j ]
-                    # deletion2 = d[k-1][i - 1][j]
-                    # mismatch = d[i - 1][j - 1]
-                    d[k][i][j] = max(insertion1, deletion1, insertion2)#, mismatch)
+                    d[k][i][j] = max(d[k][i][j - 1], d[k][i - 1][j], d[k - 1][i][j])
 
-    #return d[n][m]
+    # backtrack
+    # find k first
     dmax, pmax = -1, -1
     for k in range(1, p+1):
         if d[k][n][m] > dmax:
             dmax = d[k][n][m]
             pmax = k
 
-    # backtrack
+    # init rest
     common = [None] * dmax
     c_index = dmax - 1
     k, i, j = pmax, n, m
+    # finally, the backtrack
     while k > 0 and i > 0 and j > 0:
-        if first[j - 1] == second[i - 1] == third[k - 1]:#d[i][j] > d[i-1][j-1]:
+        if first[j - 1] == second[i - 1] == third[k - 1]:
             common[c_index], c_index = first[j - 1], c_index - 1
             k, i, j = k - 1, i - 1, j - 1
         else:
             options = [
-                (d[k-1][i][j], (k-1,i,j)),
-                (d[k][i-1][j], (k,i-1,j)),
-                (d[k][i][j-1], (k,i,j-1)),
+                (d[k - 1][i][j], (k - 1, i, j)),
+                (d[k][i - 1][j], (k, i - 1, j)),
+                (d[k][i][j - 1], (k, i, j - 1)),
             ]
             options.sort(key=lambda x: x[0], reverse=True)
-            k,i,j = options[0][1]
-            # if d[i-1][j] > d[i][j]-1:
-            #     i -= 1
-            # else:
-            #     j -= 1
+            k, i, j = options[0][1]
 
     return common
-    #return d
 
-def lcs3(a, b, c):
-    common = get_edit_distance_matrix(a,b,c)
-    return common
-    # return "".join([str(x) for x in common])
-
-test = False
-if test:
-    test_cases = [#("abcdaf", "acbcf", ""),
-                  ((['a', 'b', 'c', 'd', 'a', 'f'], ['a', 'c', 'b', 'c', 'f'], ['a', 'b', 'c', 'd', 'a', 'f']), ['a','b','c','f']),
-                  ((['a', 'b', 'c', 'd', 'a', 'f'], ['a', 'c', 'b', 'c', 'f'], ['a', 'c', 'b', 'c', 'f']), ['a','b','c','f']),
-                  ((list("123"), list("213"), list("135")), list("13")),
-                  ((list("123"), list("123"), list("123")), list("123")),
-                  ((list("123"), list("123"), list("1123")), list("123")),
-                  ((list("123"), list("12"), list("1123")), list("12")),
-                  ((list("1111"), list("2222111211"), list("11111")), list("1111")),
-                  (([8, 3, 2, 1, 7], [8, 2, 1, 3, 8, 10, 7], [6, 8, 3, 1, 4, 7]), [8, 3, 7])
-                  ]
-    for (a,b,c),expected in test_cases:
-        actual = lcs3(a,b,c)
-        if actual != expected:
-            print("FAIL. Expected: {0}, Actual: {1} for {2}".format(expected, actual, (a,b,c)))
-        else:
-            print("SUCCESS")
-else:
-    #main
-    #if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    an = data[0]
-    data = data[1:]
-    a = data[:an]
-    data = data[an:]
-    bn = data[0]
-    data = data[1:]
-    b = data[:bn]
-    data = data[bn:]
-    cn = data[0]
-    data = data[1:]
-    c = data[:cn]
-    print(len(lcs3(a, b, c)))
+#main
+input = sys.stdin.read()
+data = list(map(int, input.split()))
+an = data[0]
+data = data[1:]
+a = data[:an]
+data = data[an:]
+bn = data[0]
+data = data[1:]
+b = data[:bn]
+data = data[bn:]
+cn = data[0]
+data = data[1:]
+c = data[:cn]
+print(len(lcs3(a, b, c)))
