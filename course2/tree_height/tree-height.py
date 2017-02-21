a4a0c53e331f09a0fb037751604989885fd1146e
Aleksey Berezan <aleksey.berezan@gmail.com>
2017-02-20 23:51:14 -0500

    tree height

--- course2/tree_height/tree-height.py
+++ course2/tree_height/tree-height.py
@@ -4,26 +4,38 @@ import sys, threading
 sys.setrecursionlimit(10**7) # max depth of recursion
 threading.stack_size(2**27)  # new thread will get stack of such size
 
-class TreeHeight:
-        def read(self):
-                self.n = int(sys.stdin.readline())
-                self.parent = list(map(int, sys.stdin.readline().split()))
+class TreeNode:
+        def __init__(self, value):
+                self.value = value
+                self.children = []
+                self.parent = None
 
-        def compute_height(self):
-                # Replace this code with a faster implementation
-                maxHeight = 0
-                for vertex in range(self.n):
-                        height = 0
-                        i = vertex
-                        while i != -1:
-                                height += 1
-                                i = self.parent[i]
-                        maxHeight = max(maxHeight, height);
-                return maxHeight;
+        def get_height(self, current_height = 0):
+                if len(self.children) == 0:
+                        return current_height + 1
+                else:
+                        l = max(map(lambda c: c.get_height(current_height + 1), self.children))
+                        return l
+
+def read_nodes(s):
+        items = list(map(lambda x: int(x), s.split()))
+        nodes = [None] * len(items)
+        for i in range(0, len(items)):
+                nodes[i] = TreeNode(i)
+        for i in range(0, len(items)):
+                parent_index = items[i]
+                if(parent_index > -1):
+                        parent = nodes[parent_index]
+                        parent.children.append(nodes[i])
+                        nodes[i].parent = parent
+        return nodes
 
 def main():
-  tree = TreeHeight()
-  tree.read()
-  print(tree.compute_height())
+        ignored = input()
+        s = input()
+        nodes = read_nodes(s)
+        root_node = [n for n in nodes if n.parent == None][0]
+        height = root_node.get_height(0)
+        print(height)
 
 threading.Thread(target=main).start()
