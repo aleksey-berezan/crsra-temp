28fa70306aeb8a4d986b627a72cf6ed94ff2426c
Aleksey Berezan <aleksey.berezan@gmail.com>
2017-02-20 23:19:25 -0500

    brace check

--- course2/check_brackets_in_code/check_brackets.py
+++ course2/check_brackets_in_code/check_brackets.py
@@ -2,31 +2,52 @@
 
 import sys
 
-class Bracket:
-    def __init__(self, bracket_type, position):
-        self.bracket_type = bracket_type
-        self.position = position
+class Stack:
+    def __init__(self):
+        self.items = []
 
-    def Match(self, c):
-        if self.bracket_type == '[' and c == ']':
-            return True
-        if self.bracket_type == '{' and c == '}':
-            return True
-        if self.bracket_type == '(' and c == ')':
-            return True
-        return False
+    def count(self):
+        return len(self.items)
 
-if __name__ == "__main__":
-    text = sys.stdin.read()
+    def push(self, item):
+        self.items.append(item)
 
-    opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next == '(' or next == '[' or next == '{':
-            # Process opening bracket, write your code here
-            pass
+    def pop(self):
+        if self.count() == 0:
+            raise Exception("Stack is empty")
+        r = self.items[self.count() - 1]
+        del self.items[-1]
+        return r
 
-        if next == ')' or next == ']' or next == '}':
-            # Process closing bracket, write your code here
-            pass
+#main
+text = input()
 
-    # Printing answer, write your code here
+stack = Stack()
+open_braces = list("([{")
+close_braces = list(")]}")
+
+success = True
+for i, next in enumerate(text):
+    if next in open_braces:
+        stack.push((next,i))
+        continue
+    if next in close_braces:
+        if stack.count() == 0:
+            print(i + 1)
+            success = False
+            break
+        index = close_braces.index(next)
+        expected = open_braces[index]
+        actual = stack.pop()[0]
+        if actual != expected:
+            print(i + 1)
+            success = False
+            break
+
+if success and stack.count() > 0:
+    error_index = stack.pop()[1]
+    print(error_index + 1)
+    success = False
+
+if success:
+    print("Success")
