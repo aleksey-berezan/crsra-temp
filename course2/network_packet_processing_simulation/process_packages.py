f1270e7cc6712f3abf979ad36deefec57fbde9de
Aleksey Berezan <aleksey.berezan@gmail.com>
2017-02-22 00:44:53 -0500

    initial sim

--- course2/network_packet_processing_simulation/process_packages.py
+++ course2/network_packet_processing_simulation/process_packages.py
@@ -1,46 +1,78 @@
 # python3
 
-class Request:
-    def __init__(self, arrival_time, process_time):
-        self.arrival_time = arrival_time
-        self.process_time = process_time
+from queue import Queue
 
-class Response:
-    def __init__(self, dropped, start_time):
-        self.dropped = dropped
-        self.start_time = start_time
+Test = False
+# main
+# 13
+size, count = 2,3
+requests = [(0,1),(0,1),(0,1)]
 
-class Buffer:
-    def __init__(self, size):
-        self.size = size
-        self.finish_time_ = []
+# 15
+# size, count = 1,3
+# requests = [(0,2),(1,4),(5,3)]
 
-    def Process(self, request):
-        # write your code here
-        return Response(False, -1)
 
-def ReadRequests(count):
+Test = False
+
+# main
+if not Test:
+    size, count = map(int, input().strip().split())
+    # parse
     requests = []
     for i in range(count):
         arrival_time, process_time = map(int, input().strip().split())
-        requests.append(Request(arrival_time, process_time))
-    return requests
+        requests.append((arrival_time, process_time))
 
-def ProcessRequests(requests, buffer):
-    responses = []
-    for request in requests:
-        responses.append(buffer.Process(request))
-    return responses
+def last(q):
+    return q.queue[q.qsize() - 1]
 
-def PrintResponses(responses):
-    for response in responses:
-        print(response.start_time if not response.dropped else -1)
+# process
+# if len(requests) == 0:
+#     return
 
-if __name__ == "__main__":
-    size, count = map(int, input().strip().split())
-    requests = ReadRequests(count)
+q = Queue(size)
+responses = []# finish_time
+finish_time = 0
+last_arrival = -1
+current_time = 0
+for arrival_time, process_time in requests:
+    # if process_time == 0:
+    #     responses.append(finish_time)
+    #     continue
+    read_same = last_arrival == arrival_time
+    if read_same:
+        if q.full():
+            responses.append(-1)
+        else:
+            finish_time = max(arrival_time, finish_time)
+            responses.append(finish_time)
+            finish_time += process_time
+            q.put_nowait((finish_time, process_time))
+        continue
 
-    buffer = Buffer(size)
-    responses = ProcessRequests(requests, buffer)
+    last_arrival = arrival_time
 
-    PrintResponses(responses)
+    if q.full() and arrival_time < finish_time:
+        responses.append(-1)
+        continue
+
+    while not q.empty():
+        start_time, packet_process_time = last(q)
+        if start_time > finish_time:
+            break
+        else:
+            q.get_nowait()
+            # responses.append(start_time)
+
+    if q.full():
+        responses.append(-1)
+        continue
+
+    finish_time = max(arrival_time, finish_time)
+    responses.append(finish_time)
+    finish_time += process_time
+    q.put_nowait((finish_time, process_time))
+
+for response in responses:
+    print(response)
