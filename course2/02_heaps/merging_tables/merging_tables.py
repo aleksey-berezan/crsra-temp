e40bc9337a780ebe2a8b844933cdddfb75255a14
Aleksey Berezan <aleksey.berezan@gmail.com>
2017-03-11 22:50:04 -0500

    disjoint set

--- course2/02_heaps/merging_tables/merging_tables.py
+++ course2/02_heaps/merging_tables/merging_tables.py
@@ -2,30 +2,48 @@
 
 import sys
 
+def get_parent(parents, index):
+    traversed = []
+    current = index
+    while parents[current] != None:
+        traversed.append(current)
+        current = parents[current]
+
+    for t in traversed:
+        parents[t] = current
+    return current
+
+def merge(parents, rows, destination, source):
+    real_destination = get_parent(parents, destination)
+    real_source = get_parent(parents, source)
+
+    if real_destination == real_source:
+        return -1
+    else:
+        rows[real_destination] += rows[real_source]
+        parents[real_source] = real_destination
+        rows[real_source] = 0
+        return rows[real_destination]
+
+# main
 n, m = map(int, sys.stdin.readline().split())
-lines = list(map(int, sys.stdin.readline().split()))
-rank = [1] * n
-parent = list(range(0, n))
-ans = max(lines)
-
-def getParent(table):
-    # find parent and compress path
-    return parent[table]
-
-def merge(destination, source):
-    realDestination, realSource = getParent(destination), getParent(source)
-
-    if realDestination == realSource:
-        return False
-
-    # merge two components
-    # use union by rank heuristic 
-    # update ans with the new maximum table size
-    
-    return True
-
+rows = list(map(int, sys.stdin.readline().split()))
+parents = [None] * n
+ans = []
+last = max(rows)
+operations = []
+ops = [(3, 5),(2, 4),(1, 4),(5, 4),(5, 3)]
 for i in range(m):
     destination, source = map(int, sys.stdin.readline().split())
-    merge(destination - 1, source - 1)
-    print(ans)
-    
+    operations.append((destination - 1, source - 1))
+
+last = max(rows)
+for destination, source in operations:
+    l = merge(parents, rows, destination, source)
+    if l != -1:
+        last = max(l, last)
+
+    ans.append(last)
+
+for a in ans:
+    print(a)
