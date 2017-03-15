934b3f6401a9041623167645ac9f4b9956372a35
Aleksey Berezan <aleksey.berezan@gmail.com>
2017-03-14 23:15:59 -0400

    phonebook

--- course2/02_heaps/hashing/phone_book/phone_book.py
+++ course2/02_heaps/hashing/phone_book/phone_book.py
@@ -6,42 +6,34 @@ class Query:
         self.number = int(query[1])
         if self.type == 'add':
             self.name = query[2]
-
-def read_queries():
-    n = int(input())
-    return [Query(input().split()) for i in range(n)]
-
-def write_responses(result):
-    print('\n'.join(result))
+        else:
+            self.name = None
 
 def process_queries(queries):
+    contacts = {}
     result = []
-    # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
-    for cur_query in queries:
-        if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
-        elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
+
+    for query in queries:
+        number = query.number
+        name = query.name
+        type = query.type
+        if type == "add":
+            contacts[number] = name
+        elif type == "del":
+            if number in contacts:
+                del contacts[number]
+        elif type == "find":
+            if number in contacts:
+                result.append(contacts[number])
+            else:
+                result.append("not found")
         else:
-            response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
-            result.append(response)
+            raise Exception("Unknown command: " + type)
+
     return result
 
-if __name__ == '__main__':
-    write_responses(process_queries(read_queries()))
+n = int(input())
+queries = [Query(input().split()) for i in range(n)]
+result = process_queries(queries)
+print('\n'.join(result))
 
