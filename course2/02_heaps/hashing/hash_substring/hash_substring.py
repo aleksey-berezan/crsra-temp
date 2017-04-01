495f36f8adcbdd9a03ad6e89eea14ae447b09361
Aleksey Berezan <aleksey.berezan@gmail.com>
2017-04-01 19:55:44 -0400

    hash substring search

--- course2/02_heaps/hashing/hash_substring/hash_substring.py
+++ course2/02_heaps/hashing/hash_substring/hash_substring.py
@@ -1,18 +1,57 @@
 # python3
 
+p = 1000000007
+x = 263
+
+
 def read_input():
     return (input().rstrip(), input().rstrip())
+
 
 def print_occurrences(output):
     print(' '.join(map(str, output)))
 
+
+def hash_code(s):
+    ans = 0
+    for c in reversed(s):
+        ans = (ans * x + ord(c)) % p
+    return ans
+
+def precompute_hashes(pattern, text):
+    t_len = len(text)
+    p_len = len(pattern)
+
+    s = text[t_len - p_len:t_len]
+    h = [None] * (t_len - p_len + 1)
+    h[t_len - p_len] = hash_code(s)
+    y = 1
+    for i in range(1, p_len + 1):
+        y = (y * x) % p
+
+    for i in range(t_len - p_len - 1, -1, -1):
+        h[i] = (x * h[i + 1] + ord(text[i]) - y * ord(text[i + p_len])) % p
+    return h
+
 def get_occurrences(pattern, text):
-    return [
-        i 
-        for i in range(len(text) - len(pattern) + 1) 
-        if text[i:i + len(pattern)] == pattern
-    ]
+    t_len = len(text)
+    p_len = len(pattern)
+    if t_len < p_len:
+        return []
 
-if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
+    p_hash = hash_code(pattern)
+    h = precompute_hashes(pattern, text)
 
+    result = []
+    for i in range(0, t_len - p_len+1):
+        if p_hash != h[i]:
+            continue
+        if text[i:i + p_len] == pattern:
+            result.append(i)
+
+    return result
+
+pattern, text = read_input()
+
+occurences = get_occurrences(pattern, text)
+print_occurrences(occurences)
